292b43eac31ac22052c2b3b324ba01d0eb672987
Lxsvrd <losordatr@gmail.com>
2022-09-06 17:21:35 -0300

    first commit

---
+++ Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public Order() { 
+            Items = new List<OrderItem>(); 
+        }
+
+    }
+}
---
+++ Domain/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ProductType
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+        public ProductType()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
